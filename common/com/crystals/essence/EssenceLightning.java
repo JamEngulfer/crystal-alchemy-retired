36543699d455691c895cd9f3095f952b072f539f
JamEngulfer <JamEngulfer221@googlemail.com>
2013-04-19 22:06:33 +0100

    Changed name from 'explosive' to 'lightning' in order to reflect new essences

--- common/com/crystals/essence/EssenceLightning.java
+++ common/com/crystals/essence/EssenceLightning.java
@@ -4,7 +4,7 @@ public class EssenceLightning extends Essence {
 
     protected EssenceLightning(int id, int weight) {
         super(id, weight);
-        this.setName("explosive");
+        this.setName("lightning");
     }
 
 }
