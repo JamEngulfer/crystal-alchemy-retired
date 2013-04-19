1f01e7ac4b397bf9eb57afa5662c3a6d95bd5fea
JamEngulfer <JamEngulfer221@googlemail.com>
2013-04-19 22:07:32 +0100

    Renamed existing 'explosive' essence to 'lightning' in order to allow for addition of proper 'explosive' essence

--- common/com/crystals/essence/Essence.java
+++ common/com/crystals/essence/Essence.java
@@ -5,9 +5,17 @@ public abstract class Essence {
 
     public static final Essence[] essenceList = new Essence[256];
 
-    public static final Essence explosive = new EssenceLightning(0, 10);
+    public static final Essence lightning = new EssenceLightning(0, 10);
 
     public static final Essence fire = new EssenceLightning(1, 10);
+    
+    public static final Essence explosive = new EssenceExplosive(2, 10);
+    
+    public static final Essence healing = new EssenceHealing(3, 10);
+    
+    public static final Essence voidessence = new EssenceVoid(4, 10);
+    
+    public static final Essence growth = new EssenceGrowth(5, 10);
 
     public final int essenceID;
     private final int weight;
