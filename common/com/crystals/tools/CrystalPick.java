f4b31a6862af49b7d22e3cbf0a13a89ff8904070
JamEngulfer <JamEngulfer221@googlemail.com>
2013-05-12 17:36:24 +0100

    Added essences/abilities

--- common/com/crystals/tools/CrystalPick.java
+++ common/com/crystals/tools/CrystalPick.java
@@ -1,15 +1,18 @@
 package com.crystals.tools;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.world.World;
 
 import com.crystals.CrystalMod;
 import com.crystals.essence.Essence;
@@ -66,6 +69,136 @@ public class CrystalPick extends CrystalToolBase {
     @Override
     public void updateIcons(IconRegister iconRegister) {
         iconIndex = iconRegister.registerIcon("CrystalAlchemy:CrystalPick");
+    }
+    
+    public boolean onBlockDestroyed(ItemStack item, World world, int blockID, int x, int y, int z, EntityLiving entity)
+    {
+        if(item.stackTagCompound != null){
+            NBTTagList list = (NBTTagList) item.stackTagCompound
+                    .getTag("essence");
+            if(list != null){
+                if(item.hasTagCompound()){
+                    short eID = ((NBTTagCompound) list.tagAt(0))
+                            .getShort("essenceID");
+                    if (Essence.essenceList[eID] != null) {
+                        
+                        if (Essence.essenceList[eID].essenceID == 2) {
+                            world.createExplosion(entity, x, y, z, 3F, true);
+                        }
+                        
+                        if (Essence.essenceList[eID].essenceID == 3) {
+                            
+                            int weight = 33;
+
+                            int random = 0 + (int)(Math.random() * ((100 - 0) + 1));
+                            if(random < weight){
+                                if(entity.getHealth() < 18){
+                                    entity.setEntityHealth(entity.getHealth() + 2);
+                                }
+                                
+                                if(entity.getHealth() < 20 && entity.getHealth() > 18){
+                                    entity.setEntityHealth(entity.getHealth() + 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean canBlockBeBroken(int id){
+        
+        Block[] blockList = new Block[] {
+                Block.cobblestone, Block.stoneDoubleSlab, Block.stoneSingleSlab,
+                Block.stone, Block.sandStone, Block.cobblestoneMossy,
+                Block.oreIron, Block.blockSteel, Block.oreCoal, Block.blockGold,
+                Block.oreGold, Block.oreDiamond, Block.blockDiamond, Block.ice,
+                Block.netherrack, Block.oreLapis, Block.blockLapis,
+                Block.oreRedstone, Block.oreRedstoneGlowing, Block.rail,
+                Block.railDetector, Block.railPowered, CrystalMod.AlchemyBlock,
+                CrystalMod.AlchemyBlockEpic, CrystalMod.AlchemyBlockSuper,
+                CrystalMod.BlockDiffuser, CrystalMod.BlockImbuer,
+                CrystalMod.BlockInfuser, CrystalMod.MagicFuelBlock,
+                CrystalMod.InertOre, CrystalMod.InertOreNether 
+        };
+        
+        for(int i = 0; i < blockList.length; i++){
+            if(blockList[i].blockID == id){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean canBlockHeal(int id){
+        
+        Block[] blockList = new Block[] {
+                Block.oreIron, Block.oreCoal, 
+                Block.oreGold, Block.oreDiamond,
+                Block.oreLapis, Block.blockLapis,
+                Block.oreRedstone, Block.oreRedstoneGlowing,
+                CrystalMod.InertOre, CrystalMod.InertOreNether 
+        };
+        
+        for(int i = 0; i < blockList.length; i++){
+            if(blockList[i].blockID == id){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean onItemUse(ItemStack item, EntityPlayer player, World world, int x, int y, int z, int side, float xFloat, float yFloat, float zFloat)
+    {
+
+        Random rand = new Random();
+        if (!player.canPlayerEdit(x, y, z, side, item))
+        {
+            return false;
+        }
+        if(world.getBlockId(x, y, z) != Block.bedrock.blockID){
+            if(item.stackTagCompound != null){
+                NBTTagList list = (NBTTagList) item.stackTagCompound
+                        .getTag("essence");
+                if(list != null){
+                    if(item.hasTagCompound()){
+                        short eID = ((NBTTagCompound) list.tagAt(0))
+                                .getShort("essenceID");
+                        if (Essence.essenceList[eID] != null) {
+
+                            if (Essence.essenceList[eID].essenceID == 4) {
+                                if(canBlockBeBroken(world.getBlockId(x, y, z))){
+                                    world.destroyBlock(x, y, z, false);
+
+                                    for (int l = 0; l < 4; ++l)
+                                    {
+                                        double d0 = (double)((float)x + rand.nextFloat());
+                                        double d1 = (double)((float)y + rand.nextFloat());
+                                        double d2 = (double)((float)z + rand.nextFloat());
+                                        double d3 = 0.0D;
+                                        double d4 = 0.0D;
+                                        double d5 = 0.0D;
+
+                                        d3 = ((double)rand.nextFloat() - 0.5D) * 0.5D;
+                                        d4 = ((double)rand.nextFloat() - 0.5D) * 0.5D;
+                                        d5 = ((double)rand.nextFloat() - 0.5D) * 0.5D;
+
+                                        world.spawnParticle("portal", d0, d1, d2, d3, d4, d5);
+                                    }
+                                }
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
     }
     
     @SuppressWarnings({ "rawtypes", "unchecked" })
